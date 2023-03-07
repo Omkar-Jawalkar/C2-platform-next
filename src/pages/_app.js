d1619f6c0c2bc3dcff150735045ce0f65c27edf6
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2023-03-07 22:23:42 +0530

    Chakra Ui Package added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Footer from "../../components/common/Footer";
+import Navbar from "../../components/common/Navbar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      {" "}
+      <Navbar />
+      <Component {...pageProps} /> <Footer />
+    </ChakraProvider>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Navbar from "../../components/common/Navbar";
 import styles from "@/styles/Home.module.css";
+import Footer from "../../components/common/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <>Hello Guys</>;
+  return <>This is Home page</>;
 }
