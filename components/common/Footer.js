7ad7b97485fefd656d708a15e2111e1c0ad4474c
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2023-03-08 22:12:37 +0530

    Navbar and Footer added

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { HStack, Text } from "@chakra-ui/react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <HStack
+      position={"absolute"}
+      bottom={0}
+      width={"100%"}
+      justifyContent={"center"}
+      p={6}
+      bgGradient={"linear(to-r, #2D033B, #810CA8)"}
+      spacing={6}
+    >
+      <Text color={"whiteAlpha.900"} fontSize={"lg"}>
+        {" "}
+        Made By US{" "}
+      </Text>
+    </HStack>
+  );
 };
 
 export default Footer;
--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -1,7 +1,48 @@
 import React from "react";
+import {
+  HStack,
+  Heading,
+  VStack,
+  Button,
+  Link as ReachLink,
+  Text,
+} from "@chakra-ui/react";
+import Link from "next/link";
+
+const MenuItem = ({ children, url, name }) => {
+  return (
+    <Link as={ReachLink} href={url}>
+      <Text color={"whiteAlpha.900"} fontSize="lg">
+        {name}
+      </Text>
+    </Link>
+  );
+};
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <HStack
+      justifyContent={"space-between"}
+      p={6}
+      bgGradient="linear(to-r, #2D033B, #810CA8)"
+    >
+      <Heading color={"whiteAlpha.900"} size="lg">
+        {" "}
+        C2 Platform
+      </Heading>
+
+      <HStack spacing={12}>
+        <MenuItem url={"/home"} name={"Home"} />
+        <MenuItem url={"/dashboard"} name={"Dashboard"} />
+        <MenuItem url={"/aboutus"} name={"About Us"} />
+      </HStack>
+
+      <HStack spacing={2}>
+        <Button href="/signup"> Sign In </Button>
+        <Button href="/login"> Login </Button>
+      </HStack>
+    </HStack>
+  );
 };
 
 export default Navbar;
