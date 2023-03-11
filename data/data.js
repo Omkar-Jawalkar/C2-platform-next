43b688c7226480c812096a9f08c6f5329ae8007f
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2023-03-11 23:39:05 +0530

    Done Table part

---
+++ data/data.js
@@ -0,0 +1,37 @@
+export const StudentData = [
+  {
+    prn: "72176545K",
+    student_name: "Monkey De Luffy",
+    department: "Computer",
+    score: "3000",
+    ranking: "1",
+  },
+  {
+    prn: "72176546K",
+    student_name: "Roronona Zoro",
+    department: "Chemical",
+    score: "2800",
+    ranking: "2",
+  },
+  {
+    prn: "72176547K",
+    student_name: "Sanji Vinsmoke",
+    department: "Computer",
+    score: "2700",
+    ranking: "3",
+  },
+  {
+    prn: "72176548K",
+    student_name: "Usap",
+    department: "Civil",
+    score: "1900",
+    ranking: "4",
+  },
+  {
+    prn: "72176548K",
+    student_name: "Usap",
+    department: "Civil",
+    score: "1900",
+    ranking: "4",
+  },
+];
